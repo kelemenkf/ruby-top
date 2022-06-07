bbc7b63c466231c347dfba1ec44d63be05ea1b37
kelemenkf <kelemenkf@gmail.com>
2022-06-07 11:00:56 +0200

    something works

--- oo-projects/mastermind.rb
+++ oo-projects/mastermind.rb
@@ -4,9 +4,14 @@
 require 'colorize'
 
 class Board
-    COLORS = [''.red,''.blue,''.yellow,''.green,''.white,''.magenta]
+    @@red = ''.red
+    @@blue = ''.blue
+    @@yellow = ''.yellow
+    @@green =''.green
+    @@white = ''.white
+    @@magenta =''.magenta
+    COLORS = [@@red, @@blue, @@yellow, @@green, @@white, @@magenta]
     @@code = 4.times.map { COLORS.sample }
-    puts @@code
 
     def initialize(tries, pegs)
         @tries = tries
@@ -20,10 +25,42 @@ class Board
 
     def choose_color
         puts "Choose a color: r/b/y/g/w/m"
-        color = gets.chomp
-        case color
+        choice = gets.chomp
+        case choice
         when 'r'
+            @color_choice = @@red
+        when 'b'
+            @color_choice = @@blue
+        when 'y'
+            @color_choice = @@yellow
+        when 'g'
+            @color_choice = @@green
+        when 'w'
+            @color_choice = @@white
+        when 'm'
+            @color_choice = @@magenta
         end
+        return @color_choice
+    end
+
+    def choose_place 
+        puts "Choose a position: 1/2/3/4"
+        choice = gets.chomp
+        case choice 
+        when '1'
+            @pos_choice = 0
+        when '2'
+            @pos_choice = 1
+        when '3'
+            @pos_choice = 2
+        when '4'
+            @pos_choice = 3
+        end
+        return @pos_choice
+    end
+
+    def place_color(r=0, c, color)
+        @board[r][c] = color
     end
 end
 
@@ -32,3 +69,5 @@ game.display
 peg_board = Board.new(12, 4)
 puts "\n"
 peg_board.display
+game.place_color(game.choose_place, game.choose_color)
+game.display
