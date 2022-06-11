f3454159a626bf63b4a1f6537d4a15cbc7c5aa58
kelemenkf <kelemenkf@gmail.com>
2022-06-11 12:43:09 +0200

    tutorial

---
+++ event_manager/lib/event_manager.rb
@@ -0,0 +1,70 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def clean_phone_number(number)
+    if number.length < 10
+        'Bad number'
+    elsif number.length == 11 && number[0] == 1
+        number.shift
+    elsif number.length == 11 && number[0] != 1
+        'Bad number'
+    elsif number.length > 11
+        'Bad number'
+    else 
+        number
+    end
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts 'Event manager initialized!'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+
+  zip_code = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zip_code)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
+end
