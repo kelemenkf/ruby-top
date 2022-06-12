20f768d6a2016081f245bfcc78b8afb60fae49b0
kelemenkf <kelemenkf@gmail.com>
2022-06-12 12:55:02 +0200

    can save and reload, but only one file so far

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,94 +1,93 @@
 require 'yaml'
 
 class Hangman
-    def initialize
-        @word = @@wordlist.sample
-        @input_field = []
-        @word.length.times do 
-            @input_field.push('_ ')
-        end 
-        @input_field = "#{@input_field.join()}"
-        @game_over = false
-        @wrong_counter = 0
-        @used_letters = []
+  def initialize
+    @word = @@wordlist.sample
+    @input_field = []
+    @word.length.times do
+      @input_field.push('_ ')
+    end
+    @input_field = "#{@input_field.join}"
+    @game_over = false
+    @wrong_counter = 0
+    @used_letters = []
+    @game_saved = false
+    @game_reloaded = false
+    @letter = ''
+  end
+
+  attr_accessor :word, :input_field, :game_over, :game_saved, :wrong_counter, :used_letters, :game_reloaded, :letter
+
+  @@wordlist = []
+  lines = File.open('wordlist.txt').readlines.each do |line|
+    line = line.strip
+    @@wordlist << line if line.length > 4 && line.length < 13
+  end
+
+  def input_letter
+    @letter = gets.chomp.downcase
+    if @letter == 'save'
+      serialized = self.to_yaml
+      save(serialized)
+      @game_saved = true
+    elsif @letter == 'reload'
         @game_saved = false
+        @game_reloaded = true
+    elsif @used_letters.include?(@letter)
+      puts 'Already used!'
+    elsif @used_letters << @letter
+      @word.length.times do |i|
+        @input_field[2 * i] = @letter if @word[i] == @letter
+      end
+      unless @word.include?(@letter)
+        @wrong_counter += 1
+        puts "Number of wrong guess: #{@wrong_counter}"
+      end
     end
+  end
 
-    attr_accessor :word, :input_field, :game_over, :game_saved
-
-    @@wordlist = []
-    lines = File.open('wordlist.txt').readlines.each do |line|
-        line = line.strip
-        if line.length > 4 && line.length < 13
-            @@wordlist << line
-        end
+  def checking_winner
+    if @word == @input_field.gsub(' ', '')
+      @game_over = true
+      puts 'Game over'
+      puts "#{word}"
+    elsif @wrong_counter == 12
+      @game_over = true
+      puts 'You lose'
+      puts "The word was #{word}"
     end
+  end
 
-    def input_letter
-        letter = gets.chomp.downcase
-        if letter.length > 1
-            serialized = self.to_yaml
-            save(serialized)
-            @game_saved = true
-        else
-            if @used_letters.include?(letter)
-                puts "Already used!"
-            elsif
-                @used_letters << letter
-                @word.length.times do |i|
-                    if @word[i] == letter
-                        @input_field[2*i] = letter
-                    end
-                end
-                if !@word.include?(letter)
-                    @wrong_counter += 1
-                    puts "Number of wrong guess: #{@wrong_counter}"
-                end
-            end
-        end
+  def to_yaml
+    YAML.dump(self)
+  end
+
+  def save(serialized)
+    saved_game = File.open('saved.yaml', 'w')
+    saved_game.puts serialized
+    saved_game.close
+  end
+
+  def self.reload 
+    File.open('saved.yaml') do |f|
+        data = YAML.load(f)
+        p data
+        return data
     end
-
-    def checking_winner
-        if @word == @input_field.gsub(' ', '')
-            @game_over = true
-            puts "Game over"
-            puts "#{word}"
-        elsif
-            @wrong_counter == 12
-            @game_over = true
-            puts "You lose"
-            puts "The word was #{word}"
-        end
-    end
-
-    def to_yaml
-        YAML.dump ({
-            :word => @word,
-            :input_field => @input_field,
-            :game_over => @game_over,
-            :wrong_counter => @wrong_counter,
-            :used_letters => @used_letters
-        })
-    end
-
-    def save(serialized)
-        saved_game = File.open('saved.yaml', 'w')
-        saved_game.puts serialized
-        saved_game.close
-    end
-
+  end
 end
 
 game = Hangman.new
-puts "To save type save"
-puts game.input_field
-game.input_letter
+puts 'To save type save, to reload type reload'
+#somehow show previously saved files. type reload and filename to reload it.
 
 until game.game_over
-    puts game.input_field
-    game.input_letter
-    game.checking_winner
-    if game.game_saved
-        break
-    end
+  puts game.input_field
+  game.input_letter
+  game.checking_winner
+  if game.game_saved
+    break
+  elsif game.game_saved == false && game.game_reloaded == false && game.letter.length > 1
+    game = Hangman.reload
+  end
 end
